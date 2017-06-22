00bd5f403dd1a39445416a75fb11203319c201c7
yhoelfarfan <yhoelfarfan@YHOEL>
2017-06-22 06:01:47 -0400

    Solucion a problema con pop up

--- view/solicitudprofesorView.php
+++ view/solicitudprofesorView.php
@@ -9,9 +9,9 @@
         <style>
             .container{
                 position: absolute;
-                top: 50%;
-                left: 50%;
-                transform: translateX(-50%) translateY(-50%);   
+                top: 20%;
+                width: 100%;
+                 
             }
             footer {
                 padding-top:10px;
@@ -29,7 +29,7 @@
             <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
             <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
             <div class="container">
-            <div class="panel panel-default">
+            <div class="panel panel-default center-block">
 
                 <div class="panel-footer">
                     <form action="<?php echo $helper->url("materiales", "index"); ?>" method="post">
